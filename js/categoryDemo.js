ba44e281d25a952f8eb788c8893686a2620b4ba1
Adrian Bleoju <adrian.bleoju@yahoo.com>
2021-04-11 19:51:46 +0300

    courses-update

---
+++ js/categoryDemo.js
@@ -0,0 +1,11 @@
+// let category__button = document.getElementsByClassName("category-button");
+
+// category__button.addEventListener("click", showCategory);
+
+// function showCategory(event) {
+
+//     let category__selected = category__button.tagName;
+//     let to__show = document.getElementsByClassName(category__selected.toLowerCase);
+//     to__show.classList.remove("hide");
+
+// }
