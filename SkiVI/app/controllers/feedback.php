cb5ae73cf44a80c478261363f69ab3111028f407
Adrian Bleoju <adrian.bleoju@yahoo.com>
2021-06-22 21:47:30 +0300

    userdata

--- SkiVI/app/controllers/feedback.php
+++ SkiVI/app/controllers/feedback.php
@@ -17,75 +17,57 @@ class Feedback extends Controller
 
     function send_mail()
     {
-        if(isset($_POST['submit']))
-        {
+        if (isset($_POST['submit'])) {
             $to = "skivibagrd@gmail.com";
-            $from = "gheorghitard@gmail.com"; // inlocuit cu adresa email a utilizatorului logat
-            $name = "Bob"; // inlocuit cu numele utilizatorului
+            $logged__user = get__logged__user();
+            // $from = "gheorghitard@gmail.com";  // // inlocuit cu adresa email a utilizatorului logat
+            $from = $logged__user->email;
+            // $name = "Bob"; // inlocuit cu numele utilizatorului
+            $name = $logged__user->name;
             $subject = "Feedback from " . $name;
-            if(isset($_POST["appearance"]))
-            {
+            if (isset($_POST["appearance"])) {
                 $appearance = "Q: How do you feel about the general appearance of the website? \nA: " . $_POST["appearance"] . "\n";
-            }
-            else
-            {
+            } else {
                 $appearance = "Q: How do you feel about the general appearance of the website? \nA: Not responded\n";
             }
-            if(isset($_POST["presentation"]))
-            {
+            if (isset($_POST["presentation"])) {
                 $presentation = "Q: Do you like the way courses/lessons are presented to you? \nA: " . $_POST["presentation"] . "\n";
-            }
-            else
-            {
+            } else {
                 $presentation = "Q: Do you like the way courses/lessons are presented to you? \nA: Not responded\n";
             }
-            if(isset($_POST["diversity"]))
-            {
+            if (isset($_POST["diversity"])) {
                 $diversity = "Q: Do you think there is enough diversity in our courses/lessons? \nA: " . $_POST["diversity"] . "\n";
-            }
-            else
-            {
+            } else {
                 $diversity = "Q: Do you think there is enough diversity in our courses/lessons? \nA: Not responded\n";
             }
-            if($_POST["user_likes"]!="")
-            {
+            if ($_POST["user_likes"] != "") {
                 $user_likes = "Q: What do you like about our website? \nA: " . $_POST["user_likes"] . "\n";
-            }
-            else
-            {
+            } else {
                 $user_likes = "Q: What do you like about our website? \nA: Not responded\n";
             }
-            if($_POST["user_dislikes"]!="")
-            {
+            if ($_POST["user_dislikes"] != "") {
                 $user_dislikes = "Q: What do you dislike about our website? \nA: " . $_POST["user_dislikes"] . "\n";
-            }
-            else
-            {
+            } else {
                 $user_dislikes = "Q: What do you dislike about our website? \nA: Not responded\n";
             }
-            if($_POST["user_improvements"]!="")
-            {
+            if ($_POST["user_improvements"] != "") {
                 $user_improvements = "Q: What should we work on to improve your experience while using this website? \nA: " . $_POST["user_improvements"] . "\n";
-            }
-            else
-            {
+            } else {
                 $user_improvements = "Q: What should we work on to improve your experience while using this website? \nA: Not responded\n";
             }
-            if($_POST["user_personal"]!="")
-            {
+            if ($_POST["user_personal"] != "") {
                 $user_personal = "Q: Anything else you would like to share with us? \nA: " . $_POST["user_personal"] . "\n";
-            }
-            else
-            {
+            } else {
                 $user_personal = "Q: Anything else you would like to share with us? \nA: Not responded\n";
             }
             $message = $appearance . $presentation . $diversity . $user_likes . $user_dislikes . $user_improvements . $user_personal;
-            $headers = array("From: " . $from,
-                            "Reply-To: " . $to,
-                            "X-Mailer: PHP/" . PHP_VERSION
-                            );
+            $headers = array(
+                "From: " . $from,
+                "Reply-To: " . $to,
+                "X-Mailer: PHP/" . PHP_VERSION
+            );
             $headers = implode("\r\n", $headers);
-            mail($to,$subject,$message,$headers);
+            mail($to, $subject, $message, $headers);
             header("Location: ../feedback/thanks");
         }
     }
--- SkiVI/app/controllers/proposal.php
+++ SkiVI/app/controllers/proposal.php
@@ -17,119 +17,98 @@ class Proposal extends Controller
 
     public function send_proposal_mail()
     {
-        if(isset($_POST['submit']))
-        {
+        if (isset($_POST['submit'])) {
             $uploadStatus = 1;
-            $name = "Bob"; // inlocuit cu numele utilizatorului
+            $logged__user = get__logged__user();
+            $name = $logged__user->name;
+            // $name = "Bob"; // inlocuit cu numele utilizatorului
             $subject = "Proposal from " . $name;
             $author = $email = $type = $proposal_name = $label = $description = "";
-            if(isset($_POST["author_name"]))
-            {
+            if (isset($_POST["author_name"])) {
                 $author = "Author: " . $_POST["author_name"] . "\n";
             }
-            if(isset($_POST["author_email"]))
-            {
+            if (isset($_POST["author_email"])) {
                 $email = "Author's email: " . $_POST["author_email"] . "\n";
             }
-            if(isset($_POST["type"]))
-            {
+            if (isset($_POST["type"])) {
                 $type = "Proposal's type:" . $_POST["type"] . "\n";
             }
-            if(isset($_POST["proposal_name"]))
-            {
+            if (isset($_POST["proposal_name"])) {
                 $proposal_name = "Proposal's name: " . $_POST["proposal_name"] . "\n";
                 $subject .= " - " . $proposal_name;
             }
-            if(isset($_POST["labels"]))
-            {
+            if (isset($_POST["labels"])) {
                 $labels = "Proposal's suggested labels: " . $_POST["labels"] . "\n";
             }
-            if(isset($_POST["description"]))
-            {
+            if (isset($_POST["description"])) {
                 $description = "Proposal's description: " . $_POST["description"] . "\n";
             }
-            if(!empty($_FILES["splash_art"]["name"]))
-            {
+            if (!empty($_FILES["splash_art"]["name"])) {
                 $targetDir = "C:/Users/40787/Desktop/TW/New folder/SkiVI/public/uploads/";
                 $fileName = basename($_FILES["splash_art"]["name"]);
                 $targetFilePath = $targetDir . $fileName;
-                if(move_uploaded_file($_FILES["splash_art"]["tmp_name"],$targetFilePath))
-                {
+                if (move_uploaded_file($_FILES["splash_art"]["tmp_name"], $targetFilePath)) {
                     $uploadedSplashArt = $targetFilePath;
-                }
-                else
-                {
+                } else {
                     $uploadStatus = 0;
                 }
             }
-            if(!empty($_FILES["annex"]["name"]))
-            {
+            if (!empty($_FILES["annex"]["name"])) {
                 $targetDir = "C:/Users/40787/Desktop/TW/New folder/SkiVI/public/uploads/";
                 $fileName = basename($_FILES["annex"]["name"]);
                 $targetFilePath = $targetDir . $fileName;
-                if(move_uploaded_file($_FILES["annex"]["tmp_name"],$targetFilePath))
-                {
+                if (move_uploaded_file($_FILES["annex"]["tmp_name"], $targetFilePath)) {
                     $uploadedAnnex = $targetFilePath;
-                }
-                else
-                {
+                } else {
                     $uploadStatus = 0;
                 }
             }
-            if($uploadStatus == 1)
-            {
+            if ($uploadStatus == 1) {
                 $to = "skivibagrd@gmail.com";
-                $from = "gheorghitard@gmail.com"; // inlocuit cu adresa email a utilizatorului logat
+                // $from = "gheorghitard@gmail.com"; // inlocuit cu adresa email a utilizatorului logat
+                $from = $logged__user->email;
                 $message = $author . $email . $type . $proposal_name . $description . $labels;
                 $headers = "From: $name" . " <" . $from . ">";
-                if(!empty($uploadedSplashArt) && !empty($uploadedAnnex) && file_exists($uploadedSplashArt) && file_exists($uploadedAnnex))
-                {
+                if (!empty($uploadedSplashArt) && !empty($uploadedAnnex) && file_exists($uploadedSplashArt) && file_exists($uploadedAnnex)) {
                     $semi_rand = md5(time());
                     $mime_boundary = "==Multipart_Boundary_x{$semi_rand}x";
                     $headers .= "\nMIME-Version: 1.0\n" . "Content-Type: multipart/mixed;\n" . " boundary=\"{$mime_boundary}\"";
                     $body = "--{$mime_boundary}\n" . "Content-Type: text/plain; charset=\"UTF-8\"\n" .
-                    "Content-Transfer-Encoding: 7bit\n\n" . $message . "\n\n";
-                    if(is_file($uploadedSplashArt))
-                    {
+                        "Content-Transfer-Encoding: 7bit\n\n" . $message . "\n\n";
+                    if (is_file($uploadedSplashArt)) {
                         $body .= "--{$mime_boundary}\n";
                         $fp = @fopen($uploadedSplashArt, "rb");
                         $data = @fread($fp, filesize($uploadedSplashArt));
                         @fclose($fp);
                         $data = chunk_split(base64_encode($data));
-                        $body .= "Content-Type : application/octet-stream; name=\"" . basename($uploadedSplashArt)."\"\n" .
-                        "Content-Description: " . basename($uploadedSplashArt) . "\n" .
-                        "Content-Disposition: attachment;\n" . " filename =\"" . basename($uploadedSplashArt) . "\"; size=" . filesize($uploadedSplashArt) . ";\n" . 
-                        "Content-Transfer-Encoding: base64\n\n" . $data . "\n\n";
+                        $body .= "Content-Type : application/octet-stream; name=\"" . basename($uploadedSplashArt) . "\"\n" .
+                            "Content-Description: " . basename($uploadedSplashArt) . "\n" .
+                            "Content-Disposition: attachment;\n" . " filename =\"" . basename($uploadedSplashArt) . "\"; size=" . filesize($uploadedSplashArt) . ";\n" .
+                            "Content-Transfer-Encoding: base64\n\n" . $data . "\n\n";
                     }
-                    if(is_file($uploadedAnnex))
-                    {
+                    if (is_file($uploadedAnnex)) {
                         $body .= "--{$mime_boundary}\n";
                         $fp = @fopen($uploadedAnnex, "rb");
                         $data = @fread($fp, filesize($uploadedAnnex));
                         @fclose($fp);
                         $data = chunk_split(base64_encode($data));
-                        $body .= "Content-Type : application/octet-stream; name=\"" . basename($uploadedAnnex)."\"\n" .
-                        "Content-Description: " . basename($uploadedAnnex) . "\n" .
-                        "Content-Disposition: attachment;\n" . " filename =\"" . basename($uploadedAnnex) . "\"; size=" . filesize($uploadedSplashArt) . ";\n" . 
-                        "Content-Transfer-Encoding: base64\n\n" . $data . "\n\n";
+                        $body .= "Content-Type : application/octet-stream; name=\"" . basename($uploadedAnnex) . "\"\n" .
+                            "Content-Description: " . basename($uploadedAnnex) . "\n" .
+                            "Content-Disposition: attachment;\n" . " filename =\"" . basename($uploadedAnnex) . "\"; size=" . filesize($uploadedSplashArt) . ";\n" .
+                            "Content-Transfer-Encoding: base64\n\n" . $data . "\n\n";
                     }
 
                     $body .= "--{mime_boundary}--";
-                    mail($to,$subject,$body,$headers);
+                    mail($to, $subject, $body, $headers);
                     header("Location: ../proposal/proposal_thanks");
                     @unlink($uploadedSplashArt);
                     @unlink($uploadedAnnex);
+                } else {
+                    header("Location: ../proposal/failedlocate");
                 }
-                else
-                {
-                   header("Location: ../proposal/failedlocate");
-                }
-            }
-            else
-            {
+            } else {
                 header("Location: ../proposal/failedupload");
             }
-            
         }
     }
 
